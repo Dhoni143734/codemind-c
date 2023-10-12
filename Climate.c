bacd23d2c9f395b93df40ee5cf41f2ac98ed4a7f
Dhoni143734 <147475366+Dhoni143734@users.noreply.github.com>
2023-10-12 22:20:12 +0530

    #include<stdio.h> int main(){     int a;     scanf("%d",&a);     if(a>20)     {         printf("HOT");     }     else     {         printf("COLD");     } }

--- Climate.c
+++ Climate.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-int main (){
+#include<stdio.h>
+int main(){
     int a;
     scanf("%d",&a);
-    if (a>20)
+    if(a>20)
     {
         printf("HOT");
     }
-    else{
+    else 
+    {
         printf("COLD");
     }
 }
