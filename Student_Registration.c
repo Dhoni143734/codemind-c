c28b65e2d2d3ad192051032c6455c86f52dc11f9
Dhoni143734 <147475366+Dhoni143734@users.noreply.github.com>
2023-11-21 10:55:34 +0530

    #include<stdio.h> int main(){     int t;     scanf("%d",&t);     int a,b,c;     scanf("%d %d %d",&a,&b,&c);     if(a+c<=b){         printf("YES");     }     else if (a+c>=b){         printf("NO");     }     printf(" ");     int i,j,k;     scanf("%d %d %d",&i,&j,&k);     if(i+k<=j){         printf("YES");     }     else if (i+k>=j){         printf("NO");     }
    }

---
+++ Student_Registration.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+c<=b){
+        printf("YES");
+    }
+    else if (a+c>=b){
+        printf("NO");
+    }
+    printf("
+");
+    int i,j,k;
+    scanf("%d %d %d",&i,&j,&k);
+    if(i+k<=j){
+        printf("YES");
+    }
+    else if (i+k>=j){
+        printf("NO");
+    }
+    
+    
+    
+}
