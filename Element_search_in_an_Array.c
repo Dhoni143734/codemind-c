748f1e737e987411a6f84151bbce69d93c2700d6
Dhoni143734 <147475366+Dhoni143734@users.noreply.github.com>
2023-12-18 11:28:17 +0530

    #include<stdio.h> int main(){ int n,e,i,s=0; scanf("%d",&n); int arr[n]; for(i=0;i<n;i++){     scanf("%d",&arr[i]); } scanf("%d",&e); for(i=0;i<n;i++){   if(e==arr[i]) s=1;
    }
     if(s==1) printf("True");
        else printf("False");
    }

---
+++ Element_search_in_an_Array.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+int n,e,i,s=0;
+scanf("%d",&n);
+int arr[n];
+for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+scanf("%d",&e);
+for(i=0;i<n;i++){
+  if(e==arr[i]) s=1;
+
+}
+ if(s==1) printf("True");
+    else printf("False");
+}
